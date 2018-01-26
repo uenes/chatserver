e00a553bf27305db3ec962cb82562ac8e1142337
Uenes Ferreira <ueneslucio@gmail.com>
2018-01-25 22:54:46 -0200

    first commit

---
+++ src/main/java/com/br/uenes/chatserver/Chat/WebService/ChatWebServiceApplication.java
@@ -0,0 +1,12 @@
+package com.br.uenes.chatserver.Chat.WebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatWebServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/br/uenes/chatserver/Chat/WebService/ChatWebServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.br.uenes.chatserver.Chat.WebService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChatWebServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
